85e3943b0d8131467ed07c37e92cc3f7b9e99032
airlikidh2 <airlikidh2@gmail.com>
2016-04-01 11:51:01 -0700

    clean code

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppS
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+  console.log('%s listening to %s', server.name, server.url); 
 });
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/script-nodejs-quickstart.json
 var SCOPES = ['https://www.googleapis.com/auth/forms'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
+                 process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'script-nodejs-quickstart.json';
 
 // Load client secrets from a local file.
@@ -40,22 +40,22 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
 
 
 /**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
+* Create an OAuth2 client with the given credentials, and then execute the
+* given callback function.
+*
+* @param {Object} credentials The authorization client credentials.
+* @param {function} callback The callback to call with the authorized client.
+*/
 function authorize(credentials, callback) {
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
   var redirectUrl = credentials.installed.redirect_uris[0];
   var auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
+  
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, function(err, token) {
-	  //console.log(TOKEN_PATH);
+    //console.log(TOKEN_PATH);
     if (err) {
       getNewToken(oauth2Client, callback);
     } else {
@@ -66,13 +66,13 @@ function authorize(credentials, callback) {
 }
 
 /**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
+* Get and store new token after prompting for user authorization, and then
+* execute the given callback with the authorized OAuth2 client.
+*
+* @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+* @param {getEventsCallback} callback The callback to call with the authorized
+*     client.
+*/
 function getNewToken(oauth2Client, callback) {
   var authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
@@ -98,10 +98,10 @@ function getNewToken(oauth2Client, callback) {
 }
 
 /**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
+* Store token to disk be used in later program executions.
+*
+* @param {Object} token The token to store to disk.
+*/
 function storeToken(token) {
   try {
     fs.mkdirSync(TOKEN_DIR);
@@ -115,20 +115,20 @@ function storeToken(token) {
 }
 
 /**
- * Call an Apps Script function to list the folders in the user's root
- * Drive folder.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
+* Call an Apps Script function to list the folders in the user's root
+* Drive folder.
+*
+* @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+*/
 function callAppsScript(auth) {
   var scriptId = 'Mny6MsJjcuI5s3x7dzXquEEpkYEklKIS1';
   var script = google.script('v1');
-
+  
   // Make the API request. The request object is included here as 'resource'.
   script.scripts.run({
     auth: auth,
     resource: {
-	  devMode:true,
+      devMode:true,
       function: 'getForm'
     },
     scriptId: scriptId
@@ -140,14 +140,14 @@ function callAppsScript(auth) {
     }
     if (resp.error) {
       // The API executed, but the script returned an error.
-
+      
       // Extract the first (and only) set of error details. The values of this
       // object are the script's 'errorMessage' and 'errorType', and an array
       // of stack trace elements.
       var error = resp.error.details[0];
       console.log('Script error message: ' + error.errorMessage);
       console.log('Script error stacktrace:');
-
+      
       if (error.scriptStackTraceElements) {
         // There may not be a stacktrace if the script didn't start executing.
         for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
@@ -161,52 +161,127 @@ function callAppsScript(auth) {
       // with String keys and values, and so the result is treated as a
       // Node.js object (folderSet).
       //var items = resp.response.result;
-	  myForm = JSON.parse(resp.response.result);
+      myForm = JSON.parse(resp.response.result);
       console.log("######    Got form back from Google    ###### \n");
-	  //console.log(JSON.stringify(myForm, null, 4));
-
-	  //get the number of items in my Form
-	  nb_items = Object.keys(myForm.items).length;
-	  
-	  //create the routes for each item of the form
-	  for(i=0; i< nb_items; i++){
-			bot.add('/'+i, [
-				function (session) {
-					builder.Prompts.text(session,myForm.items[index].title ); //we ask the question from the current item
-					//TODO: ask questions differently depending on the type of the item
-				},
-				function (session, results) {
-					//we store the answers in responseReturn
-					responseReturn[index] = results.response; 
-					console.log(responseReturn[index]);
-					index++;
-					//console.log("i = "+i);
-					//console.log("index = "+index);
-					session.endDialog();
-				}
-			]);
-		}
-
-
-		bot.add('/', function (session) {
-			if (!session.userData.name) {
-				session.beginDialog('/profile');
-			} else {
-				
-				if(index < nb_items) //we start prompting the first route corresponding to the first item until there is no more
-				{
-					session.beginDialog('/'+index);
-				}
-				else{ // if there is no more items, we end the session and reset the index to 0
-					console.log("We are done!");
-					console.log(JSON.stringify(responseReturn, null, 4));
-					session.send('See ya!');
-					index=0;
-				}
-			}
-		})
-
+      //console.log(JSON.stringify(myForm, null, 4));
+      
+      //get the number of items in my Form
+      nb_items = Object.keys(myForm.items).length;
+      
+      //create the routes for each item of the form
+      for(i=0; i< nb_items; i++){
+        
+        if(myForm.items[index].type == "SECTION_HEADER"){
+          
+          bot.add('/'+i, function (session) {
+            session.send(myForm.items[index].title);
+            index++;
+            session.endDialog();
+          });
+        }			
+        else{ 
+          bot.add('/'+i, [
+                  function (session) {
+            //builder.Prompts.text(session,myForm.items[index].title ); //we ask the question from the current item
+            //TODO: ask questions differently depending on the type of the item
+            var item = myForm.items[index];
+            askItem(session,item);
+          },
+            function (session, results) {
+              //we store the answers in responseReturn
+              responseReturn[index] = results.response; 
+              //console.log(responseReturn[index]);
+              index++;
+              //console.log("i = "+i);
+              //console.log("index = "+index);
+              session.endDialog();
+            }
+          ]);
+        }
+      }
+      
+      
+      bot.add('/', function (session) {
+        if (!session.userData.name) {
+          session.beginDialog('/profile');
+        } else {
+          
+          if(index < nb_items) //we start prompting the first route corresponding to the first item until there is no more
+          {
+            session.beginDialog('/'+index);
+          }
+          else{ // if there is no more items, we end the session and reset the index to 0
+            console.log("We are done!");
+            console.log(JSON.stringify(responseReturn, null, 4));
+            session.send('See ya!');
+            index=0;
+          }
+        }
+      })
+      
     }
-
+    
   });
+}
+
+function askItem(session,item) {
+  
+  switch (item.type)  {
+      
+      
+    case "CHECKBOX":
+      builder.Prompts.choice(session, item.title, item.choices);
+      
+      break;
+      
+    case "DATE":
+      builder.Prompts.time(session, item.title);
+      
+      break;
+      
+    case "TIME":
+      
+      builder.Prompts.time(session, item.title);
+      
+      break;
+      
+    case "MULTIPLE_CHOICE":
+      builder.Prompts.choice(session, item.title, item.choices);
+      
+      break;
+      
+    case "LIST":
+      builder.Prompts.choice(session, item.title, item.choices);
+      
+      break;
+      
+    case "TEXT":
+      builder.Prompts.text(session,item.title );
+      
+      break;
+      
+    case "PARAGRAPH_TEXT":
+      builder.Prompts.text(session,item.title );  
+      
+      break;
+      
+    case "SECTION_HEADER":
+      builder.Prompts.text(session,item.title );  
+      
+      break;
+      
+    case "PAGE_BREAK":
+      
+      
+      break;
+      
+    case "SCALE":
+      builder.Prompts.number(session,"On a scale from "+item.lowerBound+" ("+item.leftLabel+") to "+item.upperBound+" ("+item.rightLabel+"), "+item.title ); 
+      
+      break;
+      
+    default:
+      Logger.log("NOT SUPPORTED AT THE MOMENT");
+      break;
+  }
 }
